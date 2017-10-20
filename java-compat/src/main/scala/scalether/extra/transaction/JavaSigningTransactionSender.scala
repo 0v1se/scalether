b78d169b2882bb5afe42a0698cf6d9a602a3b4b5
0v1se <dev@0v1se.com>
2017-10-20 11:27:45 +0300

    java compatibility

--- java-compat/src/main/scala/scalether/extra/transaction/JavaSigningTransactionSender.scala
+++ java-compat/src/main/scala/scalether/extra/transaction/JavaSigningTransactionSender.scala
@@ -4,10 +4,16 @@ import java.math.BigInteger
 import java.util.concurrent.CompletableFuture
 
 import scalether.core.JavaEthereum
+import scalether.domain.request.Transaction
 import scalether.domain.{Address, Word}
 import scalether.java.implicits._
 
 class JavaSigningTransactionSender(ethereum: JavaEthereum, from: Address, key: Word, gas: BigInteger, gasPrice: BigInteger)
   extends SigningTransactionSender[CompletableFuture](ethereum, from, key, gas, gasPrice) with JavaTransactionSender {
 
+  override def sendTransaction(transaction: Transaction) = super.sendTransaction(transaction)
+
+  override def call(transaction: Transaction) = super.call(transaction)
+
+  override protected def fill(transaction: Transaction) = super.fill(transaction)
 }
