12d74e5360babb6b7fe570152fb4ea69da547cda
0v1se <dev@0v1se.com>
2017-10-23 09:05:24 +0300

    remove unused

--- test/src/test/scala/scalether/token/Test.scala
+++
@@ -1,9 +0,0 @@
-package scalether.token
-
-import cats.implicits._
-import scalether.domain.implicits._
-
-object Test extends App with Rinkeby {
-  val token = new ExternalToken("0xfb7c1523d817bdb467bdb8c1bdc13c6f993c9e84", sender)
-  println(token.balanceOf("0x7c4890028c6f80d436de051148ed9014c1669427"))
-}
