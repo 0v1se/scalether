0c9a8e34eec7c4b913d8d02f447482be7bc097f5
0v1se <dev@0v1se.com>
2017-11-20 19:13:56 +0300

    fix tests

--- generator/src/test/java/scalether/generator/TruffleContractSpec.scala
+++ generator/src/test/java/scalether/generator/TruffleContractSpec.scala
@@ -11,7 +11,7 @@ class TruffleContractSpec extends FlatSpec {
   "TruffleContract" should "be deserialized" in {
     val token = Source.fromResource("IssuedToken.json").mkString
     val truffle = json.fromJson[TruffleContract](token)
-    assert(truffle.abi.length == 15)
+    assert(truffle.abi.length == 14)
     assert(truffle.name == "IssuedToken")
     assert(truffle.bin != null)
   }
