1fa7d1a9773820c3a3424bb86db04a9f654e9369
0v1se <dev@0v1se.com>
2017-12-15 10:53:13 +0300

    MonoGasPriceProvider added

--- java-compat/src/main/scala/scalether/extra/transaction/MonoSigningTransactionSender.scala
+++ java-compat/src/main/scala/scalether/extra/transaction/MonoSigningTransactionSender.scala
@@ -7,7 +7,7 @@ import scalether.core.MonoEthereum
 import scalether.domain.request.Transaction
 import scalether.java.implicits._
 
-class MonoSigningTransactionSender(ethereum: MonoEthereum, nonceProvider: MonoNonceProvider, privateKey: BigInteger, gas: BigInteger, gasPrice: GasPriceProvider[Mono])
+class MonoSigningTransactionSender(ethereum: MonoEthereum, nonceProvider: MonoNonceProvider, privateKey: BigInteger, gas: BigInteger, gasPrice: MonoValGasPriceProvider)
   extends SigningTransactionSender[Mono](ethereum, nonceProvider, privateKey, gas, gasPrice) with MonoTransactionSender {
 
   override def sendTransaction(transaction: Transaction): Mono[String] = super.sendTransaction(transaction)
--- java-compat/src/main/scala/scalether/extra/transaction/MonoSimpleTransactionSender.scala
+++ java-compat/src/main/scala/scalether/extra/transaction/MonoSimpleTransactionSender.scala
@@ -8,7 +8,7 @@ import scalether.domain.Address
 import scalether.domain.request.Transaction
 import scalether.java.implicits._
 
-class MonoSimpleTransactionSender(ethereum: MonoEthereum, from: Address, gas: BigInteger, gasPrice: GasPriceProvider[Mono])
+class MonoSimpleTransactionSender(ethereum: MonoEthereum, from: Address, gas: BigInteger, gasPrice: MonoGasPriceProvider)
   extends SimpleTransactionSender[Mono](ethereum, from, gas, gasPrice) with MonoTransactionSender {
 
   override def sendTransaction(transaction: Transaction): Mono[String] = super.sendTransaction(transaction)
