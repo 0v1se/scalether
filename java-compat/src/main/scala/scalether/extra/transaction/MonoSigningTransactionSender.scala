2518ccbb84d07cfb1ae0d74b80731d8613e5359c
0v1se <dev@0v1se.com>
2017-12-15 10:54:13 +0300

    MonoGasPriceProvider added

--- java-compat/src/main/scala/scalether/extra/transaction/MonoSigningTransactionSender.scala
+++ java-compat/src/main/scala/scalether/extra/transaction/MonoSigningTransactionSender.scala
@@ -7,7 +7,7 @@ import scalether.core.MonoEthereum
 import scalether.domain.request.Transaction
 import scalether.java.implicits._
 
-class MonoSigningTransactionSender(ethereum: MonoEthereum, nonceProvider: MonoNonceProvider, privateKey: BigInteger, gas: BigInteger, gasPrice: MonoValGasPriceProvider)
+class MonoSigningTransactionSender(ethereum: MonoEthereum, nonceProvider: MonoNonceProvider, privateKey: BigInteger, gas: BigInteger, gasPrice: MonoGasPriceProvider)
   extends SigningTransactionSender[Mono](ethereum, nonceProvider, privateKey, gas, gasPrice) with MonoTransactionSender {
 
   override def sendTransaction(transaction: Transaction): Mono[String] = super.sendTransaction(transaction)
