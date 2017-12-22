fadd3b62b77c85b3ab013e123ebffa4e74b451a7
0v1se <dev@0v1se.com>
2017-12-22 11:43:47 +0300

    java interop

--- java-compat/src/main/scala/scalether/extra/transaction/MonoGasPriceProvider.scala
+++ java-compat/src/main/scala/scalether/extra/transaction/MonoGasPriceProvider.scala
@@ -5,5 +5,5 @@ import java.math.BigInteger
 import reactor.core.publisher.Mono
 
 trait MonoGasPriceProvider extends GasPriceProvider[Mono] {
-  override def gasPrice: Mono[BigInteger]
+  def gasPrice: Mono[BigInteger]
 }
--- java-compat/src/main/scala/scalether/extra/transaction/MonoTransactionSender.scala
+++ java-compat/src/main/scala/scalether/extra/transaction/MonoTransactionSender.scala
@@ -1,7 +1,9 @@
 package scalether.extra.transaction
 
 import reactor.core.publisher.Mono
+import scalether.domain.request.Transaction
 
 trait MonoTransactionSender extends TransactionSender[Mono] {
-
+  def call(transaction: Transaction): Mono[String]
+  def sendTransaction(transaction: Transaction): Mono[String]
 }
