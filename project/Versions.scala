b18c31eda493e0c494027efaa0c2c7150c92ac3f
0v1se <dev@0v1se.com>
2018-02-21 23:08:11 +0100

    versions update

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
   val scala = "2.12.4"
   val slf4j = "1.7.25"
-  val asyncHttpClient = "2.2.0"
-  val reactor = "3.1.3.RELEASE"
+  val asyncHttpClient = "2.4.2"
+  val reactor = "3.1.4.RELEASE"
   val cats = "1.0.1"
-  val jackson = "2.9.1"
+  val jackson = "2.9.4"
   val scalatest = "3.0.4"
   val scalacheck = "1.13.5"
   val freemarker = "2.3.23"
